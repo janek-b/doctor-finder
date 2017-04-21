d9c9e479591fce04c75d6095fdd006b2f9f90a67
Janek Brandt <brandt.janek@gmail.com>
2017-04-21 13:53:18 -0700

    add scotch panel for doctor details

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,32 +1,54 @@
 var Doctor = require('../js/doctor.js').doctorModule;
 
-function displayDoctorList(doctorList) {
+function displayDoctorList(doctorList, detailsPanel) {
   doctorList.forEach(function(doctor) {
     var education = doctor.education.reduce((acc, ed) => acc + '<dt>'+ed.degree+'</dt><dd>'+ed.school+'</dd>', "");
     var specialties = doctor.specialties.reduce((acc, spec) => acc + ', ' + spec.name, "").slice(2);
     var newPatient;
     if(doctor.practices.filter(practice => (practice.newPatients)).map(practice => practice.newPatients).length > 0) {
-      newPatient = '<span class="pull-right label label-success">Accepting new patients</span>';
+      newPatient = '<span class="label label-success">Accepting new patients</span>';
     } else {
-      newPatient = '<span class="pull-right label label-danger">Not accepting new patients</span>';
+      newPatient = '<span class="label label-danger">Not accepting new patients</span>';
     };
     $("#doctorList").append(
       '<li class="media">'+
         '<div class="media-left">'+
-          '<a href="#">'+
+          '<a href="#" class="toggle-panel">'+
             '<img class="media-object" src="'+doctor.img+'" alt="thumbnail for doctor '+doctor.name+'">'+
           '</a>'+
         '</div>'+
         '<div class="media-body">'+
-          '<h4 class="media-heading">'+doctor.name+', '+doctor.title+newPatient+'</h4>'+
+          '<h4 class="media-heading">'+doctor.name+', '+doctor.title+' '+newPatient+'</h4>'+
           '<p>'+specialties+'</p>'+
           '<dl class="dl-horizontal">'+education+'</dl>'+
         '</div>'+
       '</li>');
+    $('#doctorList .toggle-panel').last().click(function() {
+      $("#doctorImg").html('<img src="'+doctor.img+'" alt="thumbnail for doctor '+doctor.name+'">');
+      $("#doctorName").text(doctor.name+', '+ doctor.title);
+      $("#doctorSpec").text(specialties);
+      $("#doctorBio").text(doctor.bio);
+      $("#doctorEdu").html(education);
+      detailsPanel.toggle();
+    })
   });
 }
 
 $(function() {
+  var detailsPanel = $('#details-panel').scotchPanel({
+    containerSelector: '#doctors',
+    direction: 'right',
+    duration: 300,
+    transition: 'ease',
+    clickSelector: '.toggle-panel',
+    distanceX: '80%',
+    enableEscapeKey: true
+  });
+
+  $(".overlay").click(function() {
+    detailsPanel.close();
+  })
+
   $("#findDoctorBtn").click(function() {
     var issue = $("#issue").val();
     $("#issue").val("");
@@ -40,9 +62,10 @@ $(function() {
     }
     var doctorObj = new Doctor();
     doctorObj.findDoctorByIssue(location, issue).done(function(results) {
-      displayDoctorList(results);
+      displayDoctorList(results, detailsPanel);
       console.log(results);
     })
 
   })
+
 })
