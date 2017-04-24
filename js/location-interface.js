4ab9dfa9bcde014a9fd797d71d53354fa23a1585
Janek Brandt <brandt.janek@gmail.com>
2017-04-24 15:47:46 -0700

    add info window to map markers

--- js/location-interface.js
+++ js/location-interface.js
@@ -7,12 +7,15 @@ $(function() {
 function updateMap(locations, mapCenter) {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
+    scrollwheel: false,
     center: mapCenter
   });
 
   var markers = locations.map(function(location) {
     var infowindow = new google.maps.InfoWindow({
-      content: `<h4>${location.name}</h4><h5>${location.nearestLocation.name}</h5><p>${location.nearestLocation.address.street} ${location.nearestLocation.address.city}, ${location.nearestLocation.address.state} ${location.nearestLocation.address.zip}</p>`
+      content: `<div id="content"><h2 id="firstHeading" class="firstHeading">${location.name}</h2><div class="thumbnail">`+
+        `<img src="${location.img}" alt="thumbnail for doctor ${location.name}"></div></div>`+
+        `<div id="bodyContent"><h5>${location.nearestLocation.name}</h5><p>${location.nearestLocation.address.street} ${location.nearestLocation.address.city}, ${location.nearestLocation.address.state} ${location.nearestLocation.address.zip}</p></div>`
     });
     var marker = new google.maps.Marker({
       map: map,
